4e2f722aa427024b3cad83c3e3d9d5f58c43e414
stropheum <stropheum@yahoo.com>
2018-01-28 15:54:58 -0500

    Initial Commit
    Initial project configuration for my gameboy emulator project

---
+++ Gameboy/source/Gameboy.Core.Test/stdafx.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <SDKDDKVer.h>
+
+// Headers for CppUnitTest
+#include "CppUnitTest.h"
+
+///////////////////////////////
+//// Program Dependencies ////
+/////////////////////////////
---
+++ Gameboy/source/Gameboy.Core.Test/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameboyCoreTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::Fail(L"Test not yet implemented");
+		}
+
+	};
+}
